dbd51b9588d14b7a57bfbed5929bb723d736b7d1
VitalyLitvinov74 <cheltend201@yandex.ru>
2023-08-15 21:04:44 +0500

    Нужно добавить монгу в проект. думаю надо для монги добавить файл для записи

--- src/config/components.php
+++ src/config/components.php
@@ -1,5 +1,13 @@
 <?php
 return [
+    'mongodb' => [
+        'class' => '\yii\mongodb\Connection',
+        'dsn' => 'mongodb://mongo:27017/pricecheck',
+        'options' => [
+            "username" => "root",
+            "password" => "root"
+        ]
+    ],
     'db'=>[
         'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=mysql;dbname=pricecheck',
