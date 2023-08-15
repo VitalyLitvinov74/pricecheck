aeaa7d3199f3bf4feeabeaa9a40927ed2a5aecab
VitalyLitvinov74 <cheltend201@yandex.ru>
2023-08-15 18:10:13 +0500

    Настроил xdebug

--- src/config/db.php
+++ src/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pricecheck',
+    'dsn' => 'mysql:host=mysql;dbname=pricecheck',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
