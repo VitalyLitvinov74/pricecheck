d9a02933e81e6e3ebd03fb2016ef7834fd2d2081
VitalyLitvinov74 <cheltend201@yandex.ru>
2023-11-28 10:49:51 +0500

    фиксация изменений

--- src/views/mapping-schema/create.php
+++ src/views/mapping-schema/create.php
@@ -5,12 +5,20 @@
  * @var MappingSchemaForm $form
  */
 
+use app\forms\MappingSchemaBunchForm;
 use app\forms\MappingSchemaForm;
+use wbraganca\dynamicform\DynamicFormWidget;
 use yii\web\View;
 use yii\widgets\ActiveForm;
 
 $this->title = 'Создание схемы сопоставления';
 $activeElement = ActiveForm::begin();
 echo $activeElement->field($form, 'name');
-echo $activeElement
+$bunchElement = ActiveForm::begin();
+
+
+foreach ($form->bunches as $bunch){
+
+}
+ActiveForm::end();
 ActiveForm::end();
