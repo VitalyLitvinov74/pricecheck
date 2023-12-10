b0177733b74433ea1b848e17666767af38adb559
VitalyLitvinov74 <cheltend201@yandex.ru>
2023-12-10 23:38:12 +0500

    Фиксация иземенений 2

---
+++ admin/components/Menu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navigationbar">
+    <!-- Start container-fluid -->
+    <div class="container-fluid">
+      <nav class="horizontal-nav mobile-navbar fixed-navbar">
+        <div class="collapse navbar-collapse active" id="navbar-menu">
+          <ul class="horizontal-menu in">
+            <li class="scroll dropdown">
+              <a href="" class="dropdown-toggle" data-toggle="dropdown"><img
+                  src="~/assets/images/svg-icon/dashboard.svg" class="img-fluid" alt="dashboard"><span>Dashboard</span></a>
+              <ul class="dropdown-menu animated" style="display: none; opacity: 1;">
+                <li><a href="">CRM</a></li>
+                <li><a href="">eCommerce</a></li>
+                <li><a href="">Hospital</a></li>
+                <li><a href="">Crypto</a></li>
+                <li><a href="">School</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- admin/layouts/default.vue
+++ admin/layouts/default.vue
@@ -2,28 +2,101 @@
 useHead({
   bodyAttrs: {
     class: 'horizontal-layout'
-  }
+  },
+  script: [
+    {src: "/js/jquery.min.js", body: true},
+    {src: "/js/popper.min.js", body: true},
+    {src: "/js/bootstrap.min.js", body: true},
+    {src: "/js/modernizr.min.js", body: true},
+    {src: "/js/detect.js", body: true},
+    {src: "/js/jquery.slimscroll.js", body: true},
+    {src: "/js/horizontal-menu.js", body: true},
+    {src: "/plugins/switchery/switchery.min.js", body: true},
+    {src: "/js/core.js", body: true},
+  ],
+
+  link: [
+    {rel: 'stylesheet', href: '/plugins/switchery/switchery.min.css'},
+    {rel: 'stylesheet', href: '/css/bootstrap.min.css'},
+    {rel: 'stylesheet', href: '/css/icons.css'},
+    {rel: 'stylesheet', href: '/css/flag-icon.min.css'},
+    {rel: 'stylesheet', href: '/css/style.css'},
+  ]
 })
-import '/public/plugins/switchery/switchery.min.css'
-import '/public/css/bootstrap.min.css'
-import '/public/css/icons.css'
-import '/public/css/flag-icon.min.css'
-import '/public/css/style.css'
+
 </script>
 
 <template>
-  <div id="infobar-settings-sidebar" class="infobar-settings-sidebar">
-    <div class="infobar-settings-sidebar-head d-flex w-100 justify-content-between">
+  <div>
+    <div id="infobar-settings-sidebar" class="infobar-settings-sidebar">
+      <div class="infobar-settings-sidebar-head d-flex w-100 justify-content-between">
+        <h4>Settings</h4><a href="javascript:void(0)" id="infobar-settings-close" class="infobar-settings-close"><img src="assets/images/svg-icon/close.svg" class="img-fluid menu-hamburger-close" alt="close"></a>
+      </div>
+      <div class="infobar-settings-sidebar-body">
+        <div class="custom-mode-setting">
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Payment Reminders</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-first" checked /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Stock Updates</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-second" checked /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Open for New Products</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-third" /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Enable SMS</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fourth" checked /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Newsletter Subscription</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fifth" checked /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">Show Map</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-sixth" /></div>
+          </div>
+          <div class="row align-items-center pb-3">
+            <div class="col-8"><h6 class="mb-0">e-Statement</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-seventh" checked /></div>
+          </div>
+          <div class="row align-items-center">
+            <div class="col-8"><h6 class="mb-0">Monthly Report</h6></div>
+            <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-eightth" checked /></div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="infobar-settings-sidebar-overlay"></div>
     <MenuComponent/>
     <div id="containerbar" class="container-fluid">
+
+      <div class="breadcrumbbar">
+        <div class="row align-items-center">
+          <div class="col-md-8 col-lg-8">
+            <h4 class="page-title">Basic Elements</h4>
+            <div class="breadcrumb-list">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Basic Elements</li>
+              </ol>
+            </div>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <div class="widgetbar">
+              <button class="btn btn-primary-rgba"><i class="feather icon-plus mr-2"></i>Actions</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="contentbar">
         <slot/>
       </div>
       <div class="footerbar">
         <footer class="footer">
-          <p class="mb-0">© 2020 Orbiter - All Rights Reserved.</p>
+          <p class="mb-0">© 2024 Литвинов Виталий.</p>
         </footer>
       </div>
     </div>
