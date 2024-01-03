21abc2dc9c38104fe8a976d65f351a83619fe852
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-01-04 01:13:08 +0500

    Остановился на том что нужно делать поле ввода - активным

--- admin/pages/product/new-type.vue
+++ admin/pages/product/new-type.vue
@@ -1,27 +1,98 @@
 <script setup lang="ts">
+import {fakerRU as faker} from "@faker-js/faker";
+
 const title = "Создать новый тип товаров"
-useHead({
-  title: title
+const productType = reactive({
+  title: "",
+  properties: []
 })
+
+function addProperty() {
+  productType.properties.push({name: "", isUpdating: true})
+  updateProperty(productType.properties.length-1)
+}
+
+function removeProperty(key) {
+  productType.properties.splice(key, 1)
+}
+
+function updateProperty(key) {
+  productType.properties.map(function (property) {
+    property.isUpdating = false;
+  })
+  productType.properties[key].isUpdating = true;
+  let refs = {};
+  refs[`property-` + key] = ref(null)
+  console.log(refs[`property-` + key])
+  // console.log()
+}
 </script>
 
 <template>
   <div class="row">
     <div class="col-lg-12">
-      <h5 class="card-title font-18">{{ title}}</h5>
+      <h5 class="card-title font-18">{{ title }}</h5>
+      <h6 class="card-subtitle"><code>Это тип товара, далее по полям которые вы добавите в тип будут создаваться
+        конкретные товары"</code></h6>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-12">
       <div class="card m-b-30">
         <div class="card-header">
-          <h5 class="card-title">Text</h5>
+          <h5 class="card-title">Имя типа товара (для удобства использования)</h5>
         </div>
         <div class="card-body">
-          <h6 class="card-subtitle">Usage <code>type="text"</code></h6>
           <div class="form-group mb-0">
-            <input type="text" class="form-control" name="inputText" id="inputText" placeholder="Text Input">
+            <input type="text" class="form-control" :placeholder="faker.commerce.product()" v-model="productType.title">
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-lg-12">
+      <table class="table table-striped table-bordered" id="edit-btn">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>Название свойства</th>
+          <th class="tabledit-toolbar-column"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(val, key, index) in productType.properties">
+          <td>
+            <span
+                class="tabledit-span tabledit-identifier"
+            >{{ key + 1 }}</span>
+          </td>
+          <td class="tabledit-view-mode" @click="updateProperty(key)">
+            <span class="tabledit-span" :style="val.isUpdating ? `display: none;` : ``">{{ val.name }}</span>
+            <input
+                :ref="`property-` + key"
+                class="tabledit-input form-control input-sm" type="text" v-model="val.name"
+                :style="val.isUpdating ? `` : `display: none;`"
+                :disabled="!val.isUpdating">
+          </td>
+          <td style="white-space: nowrap; width: 15%;">
+            <div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
+              <div class="btn-group btn-group-sm" style="float: none;">
+                <button type="button" class="tabledit-edit-button btn btn-sm btn-info active"
+                        style="float: none; margin: 5px;" @click="updateProperty(key)">
+                  <span class="ti-pencil"></span>
+                </button>
+                <button type="button" class="tabledit-delete-button btn btn-sm btn-info"
+                        style="float: none; margin: 5px;" @click="removeProperty(key)"><span class="ti-trash"></span>
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+
+        <div class="icon-box-list" @click="addProperty()">
+          <div>
+            <p class="m-0"><i class="ti-plus"></i></p>
+          </div>
+        </div>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
