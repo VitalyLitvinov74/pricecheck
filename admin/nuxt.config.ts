9e54d69637d3e938581c13fb5eb992a5350c697f
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-01-03 17:02:49 +0500

    Убрал config из идексирования гита убрал интернационализацию

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,20 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true, quiet: true},
-    modules: [
-        ['@nuxtjs/i18n',{
-            locales: ['ru',],
-            defaultLocale: 'ru',
-            vueI18n: {
-                fallbackLocale: 'ru',
-                messages: {
-                    ru: {
-                        route: {
-
-                        }
-                    },
-                }
-            }
-        }]
-    ],
 })
