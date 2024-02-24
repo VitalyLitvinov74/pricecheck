cb93da3da4355d21de8ffd36d840fad3a3a3ec94
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-02-24 15:54:46 +0500

    написал вложенные формы. нужно подумать над форматом ошибки.

--- backend/forms/ParsingSchemaForm.php
+++ backend/forms/ParsingSchemaForm.php
@@ -2,20 +2,47 @@
 
 namespace app\forms;
 
+use vloop\Yii2\Validators\ArrayValidator;
+use vloop\Yii2\Validators\CustomEachValidator;
 use yii\base\Model;
 
 class ParsingSchemaForm extends Model
 {
     public $name;
-    public $properties;
+
+    public $maps;
     public $productTypeId;
+
+    /** @var BunchOfPropertiesForm[] */
+    public $mapCollection;
 
     public function rules(): array
     {
         return [
-            ['name', 'string'],
-            ['productTypeId', 'string'],
-            ['map']
+            [['name', 'maps', 'productTypeId'], 'required'],
+            ['productTypeId', 'integer'],
+            ['maps', 'validateMaps'],
+            ['mapCollection', 'safe']
         ];
+    }
+
+    public function validateMaps(): void
+    {
+        $loadedBunches = [];
+        foreach ($this->maps as $bunch) {
+            $bunchForm = new BunchOfPropertiesForm();
+            $validated = $bunchForm->load($bunch) and $bunchForm->validate();
+            if ($validated) {
+                $loadedBunches[] = $bunchForm;
+                continue;
+            }
+
+        }
+        $this->mapCollection = $loadedBunches;
+    }
+
+    public function formName(): string
+    {
+        return '';
     }
 }
