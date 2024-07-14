a0fa30455f3b4005361af0124c18860a1efacfc0
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-07-14 19:31:01 +0500

    Фиксация изменений

--- backend/controllers/api/CategoryController.php
+++ backend/controllers/api/CategoryController.php
@@ -2,7 +2,59 @@
 
 namespace app\controllers\api;
 
-class CategoryController
-{
+use app\domain\ManageProductType\UseCases\ProductTypeService;
+use app\forms\ParsingSchemaForm;
+use app\forms\ProductTypeForm;
+use yii\filters\VerbFilter;
 
+class CategoryController extends BaseApiController
+{
+    private ProductTypeService $service;
+    public function behaviors(): array
+    {
+        return array_merge(parent::behaviors(), [
+            [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'product-type' => ['post'],
+                ],
+            ]
+        ]);
+    }
+
+    public function init(): void
+    {
+        $this->service = new ProductTypeService();
+        parent::init();
+    }
+
+    public function actionCreate(): array
+    {
+        $productTypeForm = new ProductTypeForm();
+        $productTypeForm->load($this->request->post());
+        if ($productTypeForm->validate()) {
+            $id = $this->service->createProductType($productTypeForm);
+            $this->jsonApi->addField('id', $id);
+            $this->jsonApi->setupCode(200);
+            return $this->jsonApi->asArray();
+        }
+        return $this->jsonApi->addModelErrors($productTypeForm)->asArray();
+    }
+
+    public function actionAddParsingSchema(): array{
+        $schemaForm = new ParsingSchemaForm();
+        $schemaForm->load($this->request->post());
+        if($schemaForm->validate()){
+            $this->service->addParsingSchemaTo(
+                $schemaForm->name,
+                $schemaForm->startWithRowNum,
+                $schemaForm->map,
+                $schemaForm->productTypeId
+            );
+            return $this->jsonApi->asArray();
+        }
+        return $this->jsonApi
+            ->addModelErrors($schemaForm)
+            ->asArray();
+    }
 }
