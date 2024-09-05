605851e6d7fb43e3987ea35377a8cd3bbbe3c73b
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-09-06 04:41:34 +0500

    фикс

--- admin/.next/static/chunks/webpack.js
+++ admin/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1387e3b8a54e5bb2"; }
+/******/ 		__webpack_require__.h = function() { return "a9f1f3bbbdbe987d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
