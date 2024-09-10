f163c6603ebbcf63ac67e5c20f64e4628e341ef2
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-09-10 23:39:34 +0500

    фикс

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -3,8 +3,25 @@ export default function RootLayout({children,}: {
 }) {
     return (
         <html lang="en">
+        <head>
+            <link href= "/assets/plugins/switchery/switchery.min.css" rel="stylesheet" />
+            <link href= "/assets/css/bootstrap.min.css" rel="stylesheet" />
+            <link href= "/assets/css/icons.css" rel="stylesheet" />
+            <link href= "/assets/css/flag-icon.min.css" rel="stylesheet" />
+            <link href= "/assets/css/style.css" rel="stylesheet" />
+
+            <script type="text/javascript" src="/assets/js/jquery.min.js"/>
+            <script type="text/javascript" src="/assets/js/popper.min.js"/>
+            <script type="text/javascript" src="/assets/js/bootstrap.min.js"/>
+            <script type="text/javascript" src="/assets/js/modernizr.min.js"/>
+            <script type="text/javascript" src="/assets/js/detect.js"/>
+            <script type="text/javascript" src="/assets/js/jquery.slimscroll.js"/>
+            <script type="text/javascript" src="/assets/js/horizontal-menu.js"/>
+            <script type="text/javascript" src="/assets/plugins/switchery/switchery.min.js"/>
+            <script type="text/javascript" src="/assets/js/core.js"/>
+        </head>
         <body>
-        {/* Layout UI */}
+
         <main>{children}</main>
         </body>
         </html>
