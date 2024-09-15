d9f0b33b344e7d16cb25d6fe084bb945f6c0bb91
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-09-15 20:32:35 +0500

    Попробовал классы - классно)

--- admin/client-components/breadcrumbs/Breadcrumbs.tsx
+++ admin/client-components/breadcrumbs/Breadcrumbs.tsx
@@ -2,31 +2,46 @@
 import React, {useContext} from "react";
 import {ClientContext} from "../layout";
 
-export default function Breadcrumbs(){
-    const {metadata} = useContext(ClientContext);
-    return (
-        <div className="breadcrumbbar">
-            <div className="row align-items-center">
-                <div className="col-md-8 col-lg-8">
-                    <h4 className="page-title">{metadata.title}</h4>
-                    <div className="breadcrumb-list">
-                        {/*<ol className="breadcrumb">*/}
-                        {/*    {breadcrumbsSettings.path.map(function(itemPath, key){*/}
-                        {/*        if(itemPath.isCurrent){*/}
-                        {/*            return (*/}
-                        {/*                <li key={key} className="breadcrumb-item" aria-current="page"><a>{itemPath.title}</a></li>*/}
-                        {/*            )*/}
-                        {/*        }*/}
-                        {/*        return (*/}
-                        {/*            <li key={key} className="breadcrumb-item">*/}
-                        {/*                <Link href={itemPath.link}>{itemPath.title}</Link>*/}
-                        {/*            </li>*/}
-                        {/*        );*/}
-                        {/*    })}*/}
-                        {/*</ol>*/}
+export default class Breadcrumbs extends React.Component<any, any> {
+    constructor({title}) {
+        super({});
+        this.state = {
+            title: title
+        }
+        this.changeTitle = this.changeTitle.bind(this)
+    }
+
+    changeTitle(newTitle :string){
+        this.setState({
+            title: newTitle
+        })
+    }
+
+    render(){
+        return (
+            <div className="breadcrumbbar">
+                <div className="row align-items-center">
+                    <div className="col-md-8 col-lg-8">
+                        <h4 className="page-title">{this.state.title}</h4>
+                        <div className="breadcrumb-list">
+                            {/*<ol className="breadcrumb">*/}
+                            {/*    {breadcrumbsSettings.path.map(function(itemPath, key){*/}
+                            {/*        if(itemPath.isCurrent){*/}
+                            {/*            return (*/}
+                            {/*                <li key={key} className="breadcrumb-item" aria-current="page"><a>{itemPath.title}</a></li>*/}
+                            {/*            )*/}
+                            {/*        }*/}
+                            {/*        return (*/}
+                            {/*            <li key={key} className="breadcrumb-item">*/}
+                            {/*                <Link href={itemPath.link}>{itemPath.title}</Link>*/}
+                            {/*            </li>*/}
+                            {/*        );*/}
+                            {/*    })}*/}
+                            {/*</ol>*/}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- admin/client-components/product-properties/product-properties-page.tsx
+++ admin/client-components/product-properties/product-properties-page.tsx
@@ -1,75 +1,154 @@
 'use client'
-import React, {useContext, useEffect, useState} from "react";
-import {ClientContext} from "../layout";
+import React from "react";
 
-export default function ProductPropertiesPage({data, title}) {
-    const {metadata, setMetadata} = useContext(ClientContext);
-    const [propertiesData, setPropertiesData] = useState(data)
-
-    const newMetadata = {title: title}
-
-    useEffect(
-        function () {
-            setMetadata(newMetadata)
-        },
-        []
-    )
-
-    function addNewRowToTable() {
-        setPropertiesData(function (prevState) {
-            return [{
-                id: 222,
-                name: "Тест",
-                type: "striing",
-            }].concat(prevState)
-        });
+export default class ProductPropertiesPage extends React.Component<any, any> {
+    constructor({data, title, breadcrumbs}) {
+        super({data, title});
+        this.state = {
+            propertiesData: data,
+        }
+        this.test = this.test.bind(this)
+        breadcrumbs.changeTitle(title);
     }
 
-    return (
-        <div className="contentbar">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="card m-b-30">
-                        <div className="card-body">
-                            <div className="table-responsive">
-                                <table className="table table-borderless table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Название</th>
-                                        <th>Тип</th>
-                                        <th>Действия</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {propertiesData.map(function (property, key) {
-                                        return (
-                                            <tr key={key}>
-                                                <td>#{key + 1}</td>
-                                                <td>{property.name}</td>
-                                                <td>
+    test() {
+        const newData = {
+            id: 222,
+            name: "Тест",
+            type: "striing",
+        };
+        this.setState({
+            propertiesData: [newData, ...this.state.propertiesData]
+        })
+    }
+
+    render() {
+        return (
+            <div className="contentbar">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="card m-b-30">
+                            <div className="card-body">
+                                <div className="btn-toolbar">
+                                    <div className="btn-group focus-btn-group">
+                                        <button onClick={this.test} type="button" className="btn btn-default"><span
+                                            className="glyphicon glyphicon-screenshot"></span>
+                                            Focus
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="table-responsive">
+                                    <table className="table table-borderless table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Название</th>
+                                            <th>Тип</th>
+                                            <th>Действия</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {this.state.propertiesData.map(function (property, key) {
+                                            return (
+                                                <tr key={key}>
+                                                    <td>#{key + 1}</td>
+                                                    <td>{property.name}</td>
+                                                    <td>
                                                     <span
                                                         className="badge badge-secondary-inverse mr-2">{property.type}</span>
-                                                </td>
-                                                <td>
-                                                    <div className="button-list">
-                                                        <a href="#" className="btn btn-success-rgba"><i
-                                                            className="feather icon-edit-2"></i></a>
-                                                        <a href="#" className="btn btn-danger-rgba"><i
-                                                            className="feather icon-trash"></i></a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                    </tbody>
-                                </table>
+                                                    </td>
+                                                    <td>
+                                                        <div className="button-list">
+                                                            <a href="#" className="btn btn-success-rgba"><i
+                                                                className="feather icon-edit-2"></i></a>
+                                                            <a href="#" className="btn btn-danger-rgba"><i
+                                                                className="feather icon-trash"></i></a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
+
+// export default function ProductPropertiesPage({data, title}) {
+//     const {metadata, setMetadata} = useContext(ClientContext);
+//     const [propertiesData, setPropertiesData] = useState(data)
+//
+//     const newMetadata = {title: title}
+//
+//     useEffect(
+//         function () {
+//             setMetadata(newMetadata)
+//         },
+//         []
+//     )
+//
+//     function addNewRowToTable() {
+//         setPropertiesData(function (prevState) {
+//             return [{
+//                 id: 222,
+//                 name: "Тест",
+//                 type: "striing",
+//             }].concat(prevState)
+//         });
+//     }
+//
+//     return (
+//         <div className="contentbar">
+//             <div className="row">
+//                 <div className="col-lg-12">
+//                     <div className="card m-b-30">
+//                         <div className="card-body">
+//                             <div className="table-responsive">
+//                                 <table className="table table-borderless table-hover">
+//                                     <thead>
+//                                     <tr>
+//                                         <th>ID</th>
+//                                         <th>Название</th>
+//                                         <th>Тип</th>
+//                                         <th>Действия</th>
+//                                     </tr>
+//                                     </thead>
+//                                     <tbody>
+//                                     {propertiesData.map(function (property, key) {
+//                                         return (
+//                                             <tr key={key}>
+//                                                 <td>#{key + 1}</td>
+//                                                 <td>{property.name}</td>
+//                                                 <td>
+//                                                     <span
+//                                                         className="badge badge-secondary-inverse mr-2">{property.type}</span>
+//                                                 </td>
+//                                                 <td>
+//                                                     <div className="button-list">
+//                                                         <a href="#" className="btn btn-success-rgba"><i
+//                                                             className="feather icon-edit-2"></i></a>
+//                                                         <a href="#" className="btn btn-danger-rgba"><i
+//                                                             className="feather icon-trash"></i></a>
+//                                                     </div>
+//                                                 </td>
+//                                             </tr>
+//                                         )
+//                                     })}
+//                                     </tbody>
+//                                 </table>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//
+//             </div>
+//         </div>
+//     )
+// }
