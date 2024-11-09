e6b79d64507e1ba1a5df038d9e7c709fafc71397
VitalyLitvinov74 <cheltend201@yandex.ru>
2024-11-09 20:57:49 +0500

    Добавил новую функцию загрузки свойств. добавил сраницу насройки таблицы

--- admin/app/products/table-settings/page.tsx
+++ admin/app/products/table-settings/page.tsx
@@ -1,7 +1,7 @@
 
 import {metadata} from "../../layout";
 import Table from "./table";
-import {loadPropertiesSettings} from "../../../utils/product-properties";
+import {loadProperties, loadPropertiesSettings} from "../../../utils/product-properties";
 
 export default async function Page(){
     metadata.title= "Настройка таблицы"
@@ -11,13 +11,18 @@ export default async function Page(){
         settings = data;
     });
 
+    let availableProperties = [];
+    await loadProperties().then(function(data){
+        availableProperties = data;
+    });
+
     return (
         <div className="contentbar">
             <div className="row">
                 <div className="col-lg-12">
                     <div className="card m-b-30">
                         <div className="card-body">
-                            <Table data={settings}/>
+                            <Table data={settings} availableProperties={availableProperties}/>
                         </div>
                     </div>
                 </div>
--- admin/app/products/table-settings/table.tsx
+++ admin/app/products/table-settings/table.tsx
@@ -1,10 +1,233 @@
-import {useState} from "react";
+'use client'
+import React, {useState} from "react";
+import Select from "react-select";
 
-export default function Table({data}) {
-    const [settings, changeTableSettings] = useState(data)
+export default function Table({data, availableProperties}) {
+
+    const [propertySettings, changePropertySettings] = useState(data)
+
+    console.log(availableProperties)
+
+    function removeRow(itemForRemove) {
+        changePropertySettings(propertySettings.filter(function (item) {
+            return itemForRemove.property_id !== item.property_id
+        }))
+        if (itemForRemove.id === null) {
+            return;
+        }
+        // const url = `http://api.pricecheck.my:82/properties/remove`;
+        // let status = 0;
+        // fetch(url, {
+        //     body: JSON.stringify({
+        //         id: itemForRemove.id
+        //     }),
+        //     headers: {
+        //         'content-type': "application/json"
+        //     },
+        //     method: "delete",
+        // }).then(function (result) {
+        //     status = result.status;
+        // })
+    }
+
+    function rollback(currentItem) {
+        changeData(data
+            .map(function (item) {
+                if (item.id === currentItem.id) {
+                    item.transactionData = {}
+                    item.isEditable = false;
+                }
+                return item;
+            })
+            .filter(function (item) {
+                if (currentItem.id === parseInt(currentItem.id)) {
+                    return true; //Оставляем только прежде добавленные
+                }
+                return currentItem.id !== item.id;
+            })
+        )
+    }
+
+    function editableRow(item) {
+        return (
+            <tr key={item.property_id}>
+                <td className="tabledit-edit-mode">
+                    <input
+                        className="tabledit-input form-control input-sm"
+                        type="text"
+                        name="name"
+                        onBlur={function (elem) {
+                            item.transactionData.name = elem.target.value
+                        }}
+                        defaultValue={item.name}
+                    />
+                </td>
+                <td className="tabledit-edit-mode">
+                    <Select
+                        options={options()}
+                        defaultValue={function () {
+                            return optionByName(item.type)
+                        }}
+                        onChange={
+                            function (option) {
+                                item.transactionData.type = option.value
+                            }
+                        }
+                    >
+                    </Select>
+                </td>
+                <td>
+                    <div className="button-list">
+                        <button type={"submit"} onClick={() => commit(item)} className="btn btn-primary-rgba">
+                            <i className="feather icon-save"></i>
+                        </button>
+                        <button type={"submit"} onClick={() => rollback(item)} className="btn btn-danger-rgba">
+                            <i className={item.id === parseInt(item.id)
+                                ? "feather icon-slash"
+                                : "feather icon-trash"}>
+                            </i>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        );
+    }
+
+    function readebleRow(item) {
+        return (
+            <tr key={item.property_id}>
+                <td>{item.property.name}</td>
+                <td>
+                    <span className="badge badge-secondary-inverse mr-2">
+                        {item.type}
+                    </span>
+                </td>
+                <td>
+                    <div className="button-list">
+                        <button type="button"
+                                onClick={
+                                    function () {
+                                        removeRow(item)
+                                    }
+                                }
+                                className="btn btn-danger-rgba">
+                            <i className="feather icon-trash"></i>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+
+    function addNewRow() {
+        const property = availableProperties.find(function (availableProperty) {
+            const setting = propertySettings.find(function (existedSetting) {
+                return existedSetting.property_id = availableProperty.id
+            })
+            if(setting){
+                return false;
+            }
+            return true;
+        })
+        console.log(property)
+        if(property == null) {
+            return;
+        }
+        const newData = {
+            property_id: property.id,
+            setting_type_id: 1,
+            property: property
+        }
+        changePropertySettings([newData, ...propertySettings]);
+    }
+
+    function options() {
+        let options: any;
+        options = availableTypes.map(function (type, key) {
+            return {value: type, label: type}
+        });
+        return options;
+    }
+
+    function optionByName(name) {
+        return options().find(function (option) {
+            return option.value === name;
+        })
+    }
+
+    function commit(updatedItem) {
+        const list = [];
+        data.map(function (item) {
+            if (item.id === updatedItem.id) {
+                item.name = updatedItem.transactionData.name;
+                item.type = updatedItem.transactionData.type;
+                item.transactionData = {};
+                item.isEditable = false
+            }
+            list.push(item)
+        })
+        changeData(list)
+        let status = 0;
+        let url = '';
+        let dataForUpsert = {};
+        if (Number.isInteger(updatedItem.id)) {
+            url = `http://api.pricecheck.my:82/properties/change`;
+            dataForUpsert = data.find(function (property) {
+                return property.id === updatedItem.id
+            })
+        } else {
+            url = `http://api.pricecheck.my:82/properties/create`;
+            dataForUpsert = {
+                properties: data.filter(function (property) {
+                    return property.id === updatedItem.id
+                })
+            }
+        }
+        console.log(url, dataForUpsert)
+
+        fetch(url, {
+            body: JSON.stringify(dataForUpsert),
+            headers: {
+                'content-type': "application/json"
+            },
+            method: "POST",
+        }).then(function (result) {
+            status = result.status;
+        })
+    }
+
     return (
-        <div>
-
-        </div>
-    )
+        <>
+            <div className="btn-toolbar">
+                <div className="btn-group focus-btn-group">
+                    <button
+                        onClick={addNewRow}
+                        type="button"
+                        className="btn btn-default">
+                        <span className="glyphicon glyphicon-screenshot"></span>
+                        Добавить
+                    </button>
+                </div>
+            </div>
+            <div className="table-responsive">
+                <table className="table table-borderless table-hover">
+                    <thead>
+                    <tr>
+                        <th width="20%">Название</th>
+                        <th width="15%">Действия</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {propertySettings.map(
+                        function (property) {
+                            if (property.isEditable) {
+                                return editableRow(property)
+                            }
+                            return readebleRow(property)
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
