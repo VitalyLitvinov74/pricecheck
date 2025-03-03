baabb61169265aa947cec7d39c69e543239efe17
VitalyLitvinov74 <cheltend201@yandex.ru>
2025-03-03 10:01:17 +0500

    добавил эластик

--- backend/infrastructure/config/components.php
+++ backend/infrastructure/config/components.php
@@ -16,5 +16,13 @@ return [
         'dsn' => 'pgsql:host=postgres;dbname=pricecheck',
         'username' => 'admin',
         'password' => 'admin',
-    ]
+    ],
+    'elasticsearch' => [
+        'class' => 'yii\elasticsearch\Connection',
+        'nodes' => [
+            ['http_address' => '127.0.0.1:9200'],
+            // configure more hosts if you have a cluster
+        ],
+        'dslVersion' => 7, // default is 5
+    ],
 ];
