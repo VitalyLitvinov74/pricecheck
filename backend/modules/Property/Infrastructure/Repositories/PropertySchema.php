908aaf400b6df7a48a8df4c6ff36d5d2ea750598
VitalyLitvinov74 <cheltend201@yandex.ru>
2025-03-15 15:42:56 +0500

    Нужно написать схему свойств

--- backend/modules/Property/Infrastructure/Repositories/PropertySchema.php
+++ backend/modules/Property/Infrastructure/Repositories/PropertySchema.php
@@ -2,12 +2,55 @@
 
 namespace app\modules\Property\Infrastructure\Repositories;
 
+use app\modules\Property\Domain\Property;
+use Cycle\ORM\Mapper\Mapper;
+use Cycle\ORM\Relation;
 use Cycle\ORM\Schema;
 
-class PropertySchema
+trait PropertySchema
 {
-    public static function build(): Schema
+    public function schema(): Schema
     {
-
+        return new Schema([
+            'property' => [
+                Schema::ENTITY => Property::class,
+                Schema::MAPPER => Mapper::class,
+//                Schema::DATABASE => 'postgres',
+                Schema::TABLE => 'properties',
+                Schema::PRIMARY_KEY => 'id',
+                Schema::COLUMNS => [
+                    'id' => 'id',
+                ],
+                Schema::TYPECAST => [
+                    'id' => 'int',
+                ],
+                Schema::RELATIONS => [
+                    'settings' => [
+                        Relation::TYPE => Relation::HAS_MANY,
+                        Relation::TARGET => 'settings',
+                        Relation::SCHEMA => [
+                            Relation::CASCADE => true,
+                            Relation::INNER_KEY => 'product_id',
+                            Relation::OUTER_KEY => 'id',
+                        ],
+                    ],
+                ],
+            ],
+            'type' => [
+                Schema::ENTITY => \app\domain\Product\Models\Property::class,
+                Schema::MAPPER => Mapper::class,
+//                Schema::DATABASE => 'postgres',
+                Schema::TABLE => 'properties',
+                Schema::PRIMARY_KEY => 'id',
+                Schema::COLUMNS => [
+                    'id' => 'id',
+                    'name' => 'name',
+                ],
+                Schema::TYPECAST => [
+                    'id' => 'int',
+                    'name' => 'string'
+                ],
+            ]
+        ]);
     }
 }
