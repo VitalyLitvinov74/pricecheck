b5b26caccc9c71f0c361d757130491e7d7e0982b
VitalyLitvinov74 <cheltend201@yandex.ru>
2025-03-30 16:41:25 +0500

    Донастроил ORM

---
+++ backend/infrastructure/migrations/m250330_110623_remove_user_id.php
@@ -0,0 +1,37 @@
+<?php
+namespace app\infrastructure\migrations;
+use yii\db\Migration;
+
+class m250330_110623_remove_user_id extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->dropColumn('properties_settings','user_id',);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->addColumn('properties_settings','user_id', $this->integer());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250330_110623_remove_user_id cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
